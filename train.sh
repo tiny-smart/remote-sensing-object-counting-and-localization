abdfc2968b5e65f5ddffb3c30a424b36a2ef0e25
zlt <1049271802@qq.com>
2024-07-16 17:02:59 +0800

    resnet path

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@ python -m torch.distributed.launch \
   --master_port=10111 \
   --use_env main.py \
   --lr=0.0001 \
-  --backbone="vgg16_bn" \
-  --pretrained="../store/pretrained/vgg16_bn-6c64b313.pth" \
+  --backbone="resnet50" \
+  --pretrained="../store/pretrained/resnet50-v1-0676ba61.pth" \
   --ce_loss_coef=1.0 \
   --point_loss_coef=5.0 \
   --eos_coef=0.5 \
